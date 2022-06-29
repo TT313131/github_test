1b00ec8f252799f3343149f1a47fbefc6a431b11
tt <trauben12@gmail.com>
2022-06-29 15:37:43 +0900

    commit!

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
-		// github practice!
+		// github practice! 뭐야 왜 안됨
 	}
 
 }
